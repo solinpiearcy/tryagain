ea693ca0ce330bd9d6de61e358cc05fa9ea10c78
solinpiearcy <solin.piearcy@menlo.edu>
2017-02-07 12:12:40 -0800

    FINALLY ran tweepy.py -had to move it to Python/2.7/site-packages though for it to run

---
+++ tweepy2results.py
@@ -0,0 +1,54 @@
+Users-MacBook-Pro-3:site-packages user$ python tweepy2.py
+/Library/Python/2.7/site-packages/requests/packages/urllib3/util/ssl_.py:334: SNIMissingWarning: An HTTPS request has been made, but the SNI (Subject Name Indication) extension to TLS is not available on this platform. This may cause the server to present an incorrect TLS certificate, which can cause validation failures. You can upgrade to a newer version of Python to solve this. For more information, see https://urllib3.readthedocs.io/en/latest/advanced-usage.html#ssl-warnings
+  SNIMissingWarning
+/Library/Python/2.7/site-packages/requests/packages/urllib3/util/ssl_.py:132: InsecurePlatformWarning: A true SSLContext object is not available. This prevents urllib3 from configuring SSL appropriately and may cause certain SSL connections to fail. You can upgrade to a newer version of Python to solve this. For more information, see https://urllib3.readthedocs.io/en/latest/advanced-usage.html#ssl-warnings
+  InsecurePlatformWarning
+ADIVINEN QUIEN VA A BAILAR COMO BEYONCÉ HOYYYYY
+Sentiment(polarity=0.0, subjectivity=0.0)
+
+RT @VraiBuzz: Scuse nous Beyoncé https://t.co/nh2ob3Mfld
+Sentiment(polarity=0.0, subjectivity=0.0)
+
+Beyonce's Photographer Explains Why It Was Hard Shooting The Singer Underwater https://t.co/NQ0ENJByUI
+Sentiment(polarity=-0.2916666666666667, subjectivity=0.5416666666666666)
+
+RT @DailyDresses: Beyoncé was slaying in this outfit https://t.co/NcOU6WxYiE
+Sentiment(polarity=0.0, subjectivity=0.0)
+
+RT @queenyonslay: Oop ellen is giving away grammy tickets to the person with the best Beyoncé costume in Texas
+Sentiment(polarity=1.0, subjectivity=0.3)
+
+RT @fieldo_: LGBT stands for Lady Gaga &amp; Beyonce - Telephone
+Sentiment(polarity=0.0, subjectivity=0.0)
+
+@PreferezVous_  Deux queen c'est dur de choisir, mais Beyoncé car j'ai grandi avec mais rihanna aussi est super👑
+Sentiment(polarity=0.0, subjectivity=0.0)
+
+Soulja Boy Pulls A Beyoncé W/ Pregnancy Announcement https://t.co/zHFTZSBeK3 https://t.co/hxCT2bWSFT
+Sentiment(polarity=0.0, subjectivity=0.0)
+
+RT @thepaxjones: @TheEllenShow is at UT doing a Beyoncé look alike contest, and of course, this is the result https://t.co/ZEI9Q0W2wY
+Sentiment(polarity=0.0, subjectivity=0.0)
+
+Beyoncé’s Full Pregnancy Shoot Has Now Been Released And It’s SO Stunning https://t.co/nNLAEH10Ni https://t.co/IDZuny56qv
+Sentiment(polarity=0.425, subjectivity=0.775)
+
+RT @FallonTonight: One of @joejonas' craziest Super Bowl bets involved high heels, dancing, &amp; @Beyonce https://t.co/1x83WXFl2Y https://t.co…
+Sentiment(polarity=0.24666666666666665, subjectivity=0.6033333333333333)
+
+RT @sweetblonde_6: Yo aún sigo esperando a que Beyonce asuma la maternidad de dinah
+
+5H3 IS COMING
+#WorkFromHome #BestMusicVideo #iHeartAwa…
+Sentiment(polarity=0.0, subjectivity=0.0)
+
+RT @kevinhelgren: @UTAustin students -- please don't wear blackface to the Beyoncé costume contest. Like, actually. That's literal racism.…
+Sentiment(polarity=0.0, subjectivity=0.1)
+
+RT @tharealversace: Beyoncé invented air don't try it https://t.co/oaDfGagtz0
+Sentiment(polarity=0.0, subjectivity=0.0)
+
+Beyonce's Photographer Explains Why It Was Hard Shooting The Singer Underwater https://t.co/kanXHUj10x
+Sentiment(polarity=-0.2916666666666667, subjectivity=0.5416666666666666)
+
+Users-MacBook-Pro-3:site-packages user$ 
